6b5baced0af82c79d9b974f328696f8d357a74a1
Kevin Verelst <kevin.verelst@euri.com>
2017-01-09 21:09:38 +0100

    Made unit tests for a new router and implemented them

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as http from 'http';
+import * as debug from 'debug';
 
 import App from './App';
 
@@ -22,4 +23,27 @@ function normalizePort(val: any|number): number {
     }
 }
 
-// TODO onError and on Listening
+function onError(error: NodeJS.ErrnoException): void {
+    if (error.syscall !== 'listen'){
+        throw error;
+    }
+    let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+    switch(error.code){
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening(): void {
+    let addr = server.address();
+    let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+    debug(`Listening on ${bind}`);
+}
